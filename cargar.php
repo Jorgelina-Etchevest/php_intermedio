61a5f45de0b6f93e7a0f103c7f35b23040c19048
Jorgelina-Etchevest <etchevestjor@gmail.com>
2022-08-20 18:57:30 -0300

    cambios

--- cargar.php
+++ cargar.php
@@ -1,40 +1,8 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./CSS/contacto.css">
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=M+PLUS+1:wght@700&family=Montserrat:ital,wght@1,200&family=Oswald&display=swap" 
-          rel="stylesheet">
-    <title>CLASES DE ECONOMÍA</title>
-    
-</head>
-
-<body>
-
-<header>
-    <nav> 
-        <ul class="linea"> 
-        <li><a href="index.php">HOME</a></li>
-        <li><a href="cursos.php">CURSOS</a></li>
-        <li><a href="contacto.php">CONTACTO</a></li>
-        <li><a href="ver.php">INSCRIPTOS</a></li>
-        <li><a href="salir.php">SALIR</a></li>
-        </ul>
-    </nav>
-
-    <h1>CLASES DE ECONOMÍA</h1>
-</header>
-
-
 <?php
 session_start();
-if(isset($_SESSION['admin'])){
-?>
+if(isset($_SESSION ['admin'])){
+
+include ('header.php')?>
 
     <section class="contactoContenido">
 
@@ -64,7 +32,8 @@ header('location: cargar.php');
 }
 ?>
 
-
+<body>
+    
 
 
 
